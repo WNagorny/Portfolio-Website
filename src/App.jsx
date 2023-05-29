6b1c74471ae96f5f2a2f094f00af5ed137fb68bf
WNagorny <walery.nagorny@gmail.com>
2023-05-29 13:10:50 +0200

    added basic styles to index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Navigation from './components/nav/Navigation'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+
+      <Header/>
+      <Navigation/>
+      <About/>
+      <Experience/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </>
   )
 }
 
