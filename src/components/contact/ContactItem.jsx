07a0e31aa5ecc239cd3de144e4e833b9c543c858
WNagorny <walery.nagorny@gmail.com>
2023-06-05 14:09:14 +0200

    finished footer section with media queries

--- src/components/contact/ContactItem.jsx
+++ src/components/contact/ContactItem.jsx
@@ -6,7 +6,7 @@ const ContactItem = ({ icon, name, info, link }) => {
          <span className='contact__option-icon'>{icon}</span>
         <h4>{name}</h4>
         <h5>{info}</h5>
-        <a href={link} target='_blank'>Send a message</a>
+        <a href={link} target='_blank' rel='noreferrer'>Send a message</a>
 		</article>
 	)
 }
